c849181af326a4c3c63785b26440ebc183570bba
ygortsf <ygordetarcio@gmail.com>
2026-02-20 23:06:18 +0000

    importação de imagem

---
+++ pdm-2026/app/index.tsx
@@ -0,0 +1,33 @@
+import { Text, View } from "react-native";
+import React from 'react';
+import {Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+export default function Index() {
+  
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>ygor</Text>
+      
+      
+    </View>
+  );
+}
